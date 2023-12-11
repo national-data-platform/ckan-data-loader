e52be51e11e4f513c9083a1d6d8b4d23431284cb
iperezx <i3perez@sdsc.edu>
2023-12-11 11:52:57 -0800

    Add harvester setup script.

--- setup_harvester.sh
+++ setup_harvester.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# This script is intended to be ran in the background
 # Wait for CKAN instance is up
 status_code=0
 while [ $status_code != 200 ]
@@ -13,7 +12,7 @@ echo "Exporting env. variables"
 export CKAN_API_TOKEN=$(awk -F "=" '/ckan.datapusher.api_token/ {print $2}' /srv/app/ckan.ini);
 export CKAN_API_URL=http://localhost:5000/api/3/action;
 
-# Run data population script
-echo "Populating data.."
-python ckan_data_loader/load_pgml_data.py;
-echo "Populating data done."
+# Run harvester setup script
+echo "Setup Harvester sources and jobs"
+python ckan_data_loader/harvester_setup.py;
+echo "Finished setting up harvester sources and jobs."
